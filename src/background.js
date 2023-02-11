fe91fa6d9ef533c0342ca458713d5edd8302da41
Manish Sewbaran <manish.sewbaran@outlook.com>
2023-02-11 15:34:40 +0100

    upgrades

--- src/background.js
+++ src/background.js
@@ -1,43 +1,49 @@
 function onCommand(command) {
-	chrome.tabs.query({url: 'https://*.spotify.com/*'}, tabs => {
-		// Open a spotify tab if one does not exist yet.
-		if (tabs.length === 0) chrome.tabs.create({url: 'https://open.spotify.com/collection/tracks'});
+    chrome.tabs.query({ url: 'https://*.spotify.com/*' }, tabs => {
+        // Open a spotify tab if one does not exist yet.
+        if (tabs.length === 0) chrome.tabs.create({ url: 'https://open.spotify.com/collection/tracks' });
 
-		for (let tab of tabs) {
-			let code = '';
+        let addLogic =
+            'var checked = btn.getAttribute("aria-checked");' +
+            'if (checked == "true") { var link = "https://www.myinstants.com/media/sounds/classic_hurt.mp3"; }' +
+            'else { var link = "https://www.myinstants.com/media/sounds/hitmarker_2.mp3"; }' +
+            'var audio = new Audio(link); audio.volume = 0.4; audio.play(); btn.click();';
 
-			if (tab.url.startsWith('https://open.spotify.com')) {
-				switch (command) {
-					case 'next':
-						code = 'document.querySelector(".bf01b0d913b6bfffea0d4ffd7393c4af-scss").click()';
-						break;
-					case 'previous':
-						code = 'document.querySelector(".bc13c597ccee51a09ec60253c3c51c75-scss").click()';
-						break;
-					case 'shuffle':
-						code = 'document.querySelector("._39234eb5c173f8b6de80ed73820b1be8-scss").click()';
-						break;
-					case 'repeat':
-						code = 'document.querySelector(".ebfd411a126f1e7bea6133f21b4ef88e-scss").click()';
-						break;
-					case 'track-add':
-						code = 'var btn = document.querySelector("._07bed3a434fa59aa1852a431bf2e19cb-scss"); var checked = btn.getAttribute("aria-checked"); if (checked == "true") { var link = "https://www.myinstants.com/media/sounds/classic_hurt.mp3"; } else { var link = "https://www.myinstants.com/media/sounds/hitmarker_2.mp3"; } var audio = new Audio(link); audio.volume = 0.4; audio.play(); btn.click();';
-						break;
-					case 'play-pause':
-						code = 'document.querySelector("._82ba3fb528bb730b297a91f46acd37a3-scss").click()';
-						break;
-				}
-			}
+        for (let tab of tabs) {
+            let code = '';
 
-			// Apply command on only 1 spotify tab.
-			if (code.length) {
-				chrome.tabs.executeScript(tab.id, {code: code});
-				break;
-			}
-		}
-		// Unload background page as soon as we're done.
-		window.close();
-	});
+            if (tab.url.startsWith('https://open.spotify.com')) {
+                switch (command) {
+                    case 'previous':
+                        code = 'document.querySelector("button[aria-label=\'Vorige\']").click()';
+                        break;
+                    case 'next':
+                        code = 'document.querySelector("button[data-testid=\'control-button-skip-forward\']").click();';
+                        break;
+                    case 'shuffle':
+                        code = 'document.querySelector("button[data-testid=\'control-button-shuffle\']").click();';
+                        break;
+                    case 'repeat':
+                        code = 'document.querySelector("button[data-testid=\'control-button-repeat\']").click()';
+                        break;
+                    case 'track-add':
+                        code = 'var btn = document.querySelector("button[data-testid=\'add-button\']");' + addLogic;
+                        break;
+                    case 'play-pause':
+                        code = 'document.querySelector("button[data-testid=\'control-button-playpause\']").click();';
+                        break;
+                }
+            }
+
+            // Apply command on only 1 spotify tab.
+            if (code.length) {
+                chrome.tabs.executeScript(tab.id, { code: code });
+                break;
+            }
+        }
+        // Unload background page as soon as we're done.
+        window.close();
+    });
 }
 
 chrome.commands.onCommand.addListener(onCommand);
