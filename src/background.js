da1f22bcc4ee9fa68bcb77104f57f989c6d97d96
Manish <rijstmetbhaat@outlook.com>
2023-03-13 13:04:48 +0100

    added new cool buttons and logic

--- src/background.js
+++ src/background.js
@@ -3,34 +3,41 @@ function onCommand(command) {
         // Open a spotify tab if one does not exist yet.
         if (tabs.length === 0) chrome.tabs.create({ url: 'https://open.spotify.com/collection/tracks' });
 
-        let addLogic =
+        let checkedOrNot =
             'var checked = btn.getAttribute("aria-checked");' +
             'if (checked == "true") { var link = "https://www.myinstants.com/media/sounds/classic_hurt.mp3"; }' +
             'else { var link = "https://www.myinstants.com/media/sounds/hitmarker_2.mp3"; }' +
             'var audio = new Audio(link); audio.volume = 0.4; audio.play(); btn.click();';
+
+        let isSongLikedLogic = 
+            'var checked = btn.getAttribute("aria-checked");' +
+            'if (checked == "true") { var link = "https://www.myinstants.com/media/sounds/yeay.mp3"; }' +
+            'else { var link = "https://www.myinstants.com/media/sounds/engineer_no01.mp3"; }' +
+            'var audio = new Audio(link); audio.volume = 0.3; audio.play();';
+
 
         for (let tab of tabs) {
             let code = '';
 
             if (tab.url.startsWith('https://open.spotify.com')) {
                 switch (command) {
-                    case 'previous':
+                    case 'a-previous':
                         code = 'document.querySelector("button[aria-label=\'Vorige\']").click()';
                         break;
-                    case 'next':
+                    case 'b-play-pause':
+                        code = 'document.querySelector("button[data-testid=\'control-button-playpause\']").click();';
+                        break;
+                    case 'c-next':
                         code = 'document.querySelector("button[data-testid=\'control-button-skip-forward\']").click();';
                         break;
-                    case 'shuffle':
-                        code = 'document.querySelector("button[data-testid=\'control-button-shuffle\']").click();';
+                    case 'd-like-song':
+                        code = 'var btn = document.querySelector("button[data-testid=\'add-button\']");' + checkedOrNot;
                         break;
-                    case 'repeat':
-                        code = 'document.querySelector("button[data-testid=\'control-button-repeat\']").click()';
+                    case 'e-is-song-liked':
+                        code = 'var btn = document.querySelector("button[data-testid=\'add-button\']");' + isSongLikedLogic;
                         break;
-                    case 'track-add':
-                        code = 'var btn = document.querySelector("button[data-testid=\'add-button\']");' + addLogic;
-                        break;
-                    case 'play-pause':
-                        code = 'document.querySelector("button[data-testid=\'control-button-playpause\']").click();';
+                    case 'f-shuffle':
+                        code = 'var btn = document.querySelector("button[data-testid=\'control-button-shuffle\']");' + checkedOrNot;
                         break;
                 }
             }
